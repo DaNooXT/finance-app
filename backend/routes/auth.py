2c5e62ad4a5c39c931a649b8aed92e3f3080cc57
DaNooXT <raulkbauer2009@gmail.com>
2026-06-26 23:25:18 -0300

    Refactor user registration: enhance error handling and update response structure

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,7 +1,30 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from backend.database.models import User
 from backend.core.dependencies import create_session
-from backend.schemas.AuthSchema import UsuarioSchema
+from backend.schemas.AuthSchema import UserSchema, ResponseUser
 
 auth_route = APIRouter(prefix="/auth", tags=["auth"])
+
+@auth_route.post("/register", response_model=ResponseUser)
+async def register (user: UserSchema, session: Session = Depends(create_session)):
+    
+    existing_user = session.query(User).filter(User.name == user.name).first()
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Usuario existente")
+    
+    try:
+        new_user = User(name=user.name, password=user.password)
+        session.add(new_user)
+        session.commit()
+        session.refresh(new_user)
+    except:
+        session.rollback()
+        raise HTTPException(status_code=500, detail="Erro interno")
+    
+    return {
+        "id": new_user.id,
+        "name": new_user.name,
+        "description": "User successfully registered"
+    }
--- backend/schemas/AuthSchema.py
+++ backend/schemas/AuthSchema.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel
 
-class UsuarioSchema (BaseModel):
+class UserSchema (BaseModel):
+    name: str
+    password: str
+
+    class Config:
+        from_attributes = True
+
+class ResponseUser (BaseModel):
+    id: int
     name: str
 
     class Config:
