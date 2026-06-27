d9e90f7c7bf4232de62d08049fdb41cab0a1b859
DaNooXT <raulkbauer2009@gmail.com>
2026-06-27 00:30:56 -0300

    Fixes typos and improves code structure: renames 'movimentaion_route' to 'movimentation_route', adds exception handling to the movement route, and updates the response schema to include the movement date.

--- backend/routes/movimentations.py
+++ backend/routes/movimentations.py
@@ -1,10 +1,19 @@
-from fastapi import APIRouter, Depends 
-# from sqlalchemy.orm import Session
-# from backend.core.dependencies import create_session
-# from schemas.MovimentationSchema import MovimentationSchema
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from backend.database.models import Movimentations
+from backend.core.dependencies import create_session
+from backend.schemas.MovimentationSchema import MovimentationSchema, ResponseMovimentation
 
-movimentaion_route = APIRouter(prefix="/movimentation", tags=["movimentaion"])
+movimentation_route = APIRouter(prefix="/movimentation", tags=["movimentaion"])
 
-# @movimentaion_route.post()
-# async def movimentation (movimentation: MovimentationSchema, seesion: Session = Depends(create_session)):
-#     pass
+@movimentation_route.post("/movimentation", response_model=ResponseMovimentation)
+async def movimentation (movimentation: MovimentationSchema, session: Session = Depends(create_session)):
+    new_movimentation = Movimentations(**movimentation.model_dump())
+    try:
+        session.add(new_movimentation)
+        session.commit()
+        session.refresh(new_movimentation)
+    except:
+        raise HTTPException(status_code=500, detail="Internal error")
+    
+    return new_movimentation
--- backend/schemas/MovimentationSchema.py
+++ backend/schemas/MovimentationSchema.py
@@ -1,12 +1,26 @@
 from pydantic import BaseModel 
 from backend.database.enums import ExpenseCategory, MovimentationType
 from decimal import Decimal
+from datetime import datetime
 
 class MovimentationSchema (BaseModel):
+    user_id: int
     amount: Decimal
     description: str
     type: ExpenseCategory
     movimentation_type: MovimentationType
+
+    class Config:
+        from_attributes = True
+
+class ResponseMovimentation (BaseModel):
+    id: int
+    user_id: int
+    amount: Decimal
+    description: str
+    type: ExpenseCategory
+    movimentation_type: MovimentationType
+    movimentation_date: datetime
 
     class Config:
         from_attributes = True
