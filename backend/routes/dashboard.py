db8808336b83d36dcd6069dee58e1e063608e45e
DaNooXT <raulkbauer2009@gmail.com>
2026-07-04 16:55:49 -0300

    Refactor dashboard endpoint: streamline expense calculations by category and update response structure

--- backend/routes/dashboard.py
+++ backend/routes/dashboard.py
@@ -9,7 +9,7 @@ dashboard_route = APIRouter(prefix="/dashboard", tags=["dashboard"])
 
 @dashboard_route.get("/get_dashboard/month")
 async def get_dashboard_month (session: Session = Depends(create_session)):
-    
+
     initial, end = get_current_month()
 
     all_movimentation = session.query(Movimentations).filter(
@@ -28,169 +28,52 @@ async def get_dashboard_month (session: Session = Depends(create_session)):
 
     month_balence = total_income - total_expense
 
-    food_expense = sum(
-        movimentation.amount                
-        for movimentation in all_movimentation
-        if movimentation.movimentation_type == MovimentationType.EXPENSE 
-        and movimentation.type == MovimentationCategories.FOOD
-    )
-    
-    food_porcent_expense = (
-        (food_expense * 100) / total_expense
-        if food_expense > 0
-        else 0
+    categories = {}
+
+    for category in MovimentationCategories:
+        if category == MovimentationCategories.SALARY:
+            continue
+
+        expense = sum(
+            movimentation.amount
+            for movimentation in all_movimentation
+            if movimentation.movimentation_type == MovimentationType.EXPENSE
+            and type == category
+        )
+
+        percentage = (
+            (expense * 100 / total_expense)
+            if expense > 0 
+            else 0
+        )
+
+        categories[category.value] = {
+            "expense": expense,
+            "percentege": percentage
+        }
+
+    len_movimentation = len(all_movimentation)
+
+    top_movimentation =  max(
+        (
+            movimentation
+            for movimentation in all_movimentation
+            if movimentation.movimentation_type == MovimentationType.EXPENSE
+        ),
+
+        key= lambda movimentation: movimentation.amount,
+        default=None
     )
 
-    transport_expense = sum(
-        movimentation.amount
-        for movimentation in all_movimentation
-        if movimentation.movimentation_type == MovimentationType.EXPENSE
-        and movimentation.type == MovimentationCategories.TRANSPORT
-    )
-
-    transport_porcent_expense = (
-        (transport_expense * 100) / total_expense
-        if transport_expense > 0
-        else 0
-    )
-
-    house_expense = sum(
-        movimentation.amount
-        for movimentation in all_movimentation
-        if movimentation.movimentation_type == MovimentationType.EXPENSE
-        and movimentation.type == MovimentationCategories.HOUSE
-    )
-
-    house_porcent_expense = (
-        (house_expense * 100) / total_expense
-        if house_expense > 0
-        else 0
-    )
-
-    health_expense = sum(
-        movimentation.amount
-        for movimentation in all_movimentation
-        if movimentation.movimentation_type == MovimentationType.EXPENSE
-        and movimentation.type == MovimentationCategories.HEALTH
-    )
-
-    health_porcent_expense = (
-        (health_expense * 100) / total_expense
-        if health_expense > 0
-        else 0
-    )
-
-    leisure_expense = sum(
-        movimentation.amount
-        for movimentation in all_movimentation
-        if movimentation.movimentation_type == MovimentationType.EXPENSE
-        and movimentation.type == MovimentationCategories.LEISURE
-    )
-
-    leisure_porcent_expense = (
-        (leisure_expense * 100) / total_expense
-        if leisure_expense > 0
-        else 0
-    )
-
-    subscriptions_expense = sum(
-        movimentation.amount
-        for movimentation in all_movimentation
-        if movimentation.movimentation_type == MovimentationType.EXPENSE
-        and movimentation.type == MovimentationCategories.SUBSCRIPTIONS
-    )
-
-    subscriptions_porcent_expense = (
-        (subscriptions_expense * 100) / total_expense
-        if subscriptions_expense > 0
-        else 0
-    )
-
-    clothing_expense = sum(
-        movimentation.amount
-        for movimentation in all_movimentation
-        if movimentation.movimentation_type == MovimentationType.EXPENSE
-        and movimentation.type == MovimentationCategories.CLOTHING
-    )
-
-    clothing_porcent_expense = (
-        (clothing_expense * 100) / total_expense
-        if clothing_expense > 0
-        else 0
-    )
-
-    pets_expense = sum(
-        movimentation.amount
-        for movimentation in all_movimentation
-        if movimentation.movimentation_type == MovimentationType.EXPENSE
-        and movimentation.type == MovimentationCategories.PETS
-    )
-
-    pets_porcent_expense = (
-        (pets_expense * 100) / total_expense
-        if pets_expense > 0
-        else 0
-    )
-
-    gifts_expense = sum(
-        movimentation.amount
-        for movimentation in all_movimentation
-        if movimentation.movimentation_type == MovimentationType.EXPENSE
-        and movimentation.type == MovimentationCategories.GIFTS
-    )
-
-    gifts_porcent_expense = (
-        (gifts_expense * 100) / total_expense
-        if gifts_expense > 0
-        else 0
-    )
-
-    others_expense = sum(
-        movimentation.amount
-        for movimentation in all_movimentation
-        if movimentation.movimentation_type == MovimentationType.EXPENSE
-        and movimentation.type == MovimentationCategories.OTHERS
-    )
-
-    others_porcent_expense = (
-        (others_expense * 100) / total_expense
-        if others_expense > 0
-        else 0
-    )
-    
     return {
-    "all_movimentation": all_movimentation,
-    "month_balence": month_balence,
-    "total_expense": total_expense,
-    "total_income": total_income,
-
-    "food_expense": food_expense,
-    "food_porcent_expense": food_porcent_expense,
-
-    "transport_expense": transport_expense,
-    "transport_porcent_expense": transport_porcent_expense,
-
-    "house_expense": house_expense,
-    "house_porcent_expense": house_porcent_expense,
-
-    "health_expense": health_expense,
-    "health_porcent_expense": health_porcent_expense,
-
-    "leisure_expense": leisure_expense,
-    "leisure_porcent_expense": leisure_porcent_expense,
-
-    "subscriptions_expense": subscriptions_expense,
-    "subscriptions_porcent_expense": subscriptions_porcent_expense,
-
-    "clothing_expense": clothing_expense,
-    "clothing_porcent_expense": clothing_porcent_expense,
-
-    "pets_expense": pets_expense,
-    "pets_porcent_expense": pets_porcent_expense,
-
-    "gifts_expense": gifts_expense,
-    "gifts_porcent_expense": gifts_porcent_expense,
-
-    "others_expense": others_expense,
-    "others_porcent_expense": others_porcent_expense,
+        "summary": {
+                "month_balence": month_balence,
+                "total_expense": total_expense,
+                "total_income": total_income,
+        },
+        "categories" : categories,
+        "statistics": {
+            "len_movimentation": len_movimentation,
+            "top_movimentation": top_movimentation
+        }
     }
